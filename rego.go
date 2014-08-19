806d021eec552c579b9025695d5c7ec613af96a5
Luke Mauldin <lmauldin@pioneeringevolution.com>
2014-08-19 14:53:53 -0500

    Refactored regex eval into seperate file

--- rego.go
+++ rego.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"regexp"
-	"time"
 
 	"appengine"
-	"appengine/datastore"
 )
 
 // MatchResultResponse is a json type
 type MatchResultResponse struct {
 	Matches    [][]string `json:"matches"`
 	GroupsName []string   `json:"groupsName"`
-}
-
-type RegexStats struct {
-	RegexLength      int
-	TextStringLength int
-	MatchDuration    time.Duration
 }
 
 func regExpHandler(rw http.ResponseWriter, req *http.Request) {
@@ -39,49 +29,22 @@ func regExpHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	startRegex := time.Now()
-
-	r, err := regexp.Compile(inputData.Regexp)
-	if err != nil {
-		ctx.Errorf("Invalid RegExp : %s \n", inputData.Regexp)
-		http.Error(rw, fmt.Sprintf("Invalid RegExp : %s", inputData.Regexp), http.StatusInternalServerError)
-		return
-	}
-
 	numMatches := -1
 	if !inputData.FindAllSubmatch {
 		numMatches = 1
 	}
-	matches := r.FindAllStringSubmatch(inputData.Text, numMatches)
-
-	m := &MatchResultResponse{}
-	if len(matches) > 0 {
-		m.Matches = matches
-		m.GroupsName = r.SubexpNames()[1:]
+	result, err := EvalRegex(ctx, inputData.Regexp, inputData.Text, numMatches)
+	if err != nil {
+		ctx.Errorf("%v", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	regexDuration := time.Since(startRegex)
-	ctx.Infof("regex duration: %v", regexDuration)
-	stats := &RegexStats{
-		RegexLength:      len(inputData.Regexp),
-		TextStringLength: len(inputData.Text),
-		MatchDuration:    regexDuration,
-	}
-	writeStatsEntry(ctx, stats)
-
-	enc := json.NewEncoder(rw)
-	err = enc.Encode(m)
+	err = json.NewEncoder(rw).Encode(result)
 	if err != nil {
 		ctx.Errorf("JSON encoding err: %v", err)
 		http.Error(rw, "JSON encoding error", http.StatusInternalServerError)
 		return
-	}
-}
-
-func writeStatsEntry(ctx appengine.Context, stats *RegexStats) {
-	_, err := datastore.Put(ctx, datastore.NewIncompleteKey(ctx, "regex_stats", nil), stats)
-	if err != nil {
-		ctx.Errorf("Error writing stats: %v", err)
 	}
 }
 
